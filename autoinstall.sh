4265cb9712426267a7ba48b143d2d70ad5a0db27
Minh Phan <mpflamingmarshmallow@gmail.com>
2024-05-15 19:15:21 -1000

    Installed lightdm, awesomewm and enabled it on boot

--- autoinstall.sh
+++ autoinstall.sh
@@ -10,3 +10,17 @@ sudo pacman -S git --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay 
 makepkg -si --noconfirm 
+
+## Install Vim (*temp) for text/config editing
+sudo yay -S vim --noconfirm
+
+## Install LightDM display manager
+sudo yay -S xorg-server lightdm lightdm-gtk-greeter
+
+## TODO: Copy light dm config file over 
+
+## Install awesome
+sudo yay -S xorg-init xterm awesome
+
+## Enable lightdm
+sudo systemctl enable lightdm
---
+++ notes.md
@@ -0,0 +1,8 @@
+### Light DM Display Manager Greeters
+- lightdm-gtk-greeter
+- lightdm-pantheon-greeter
+- lightdm-slick-greeter
+- lightdm-webkit2-greeter
+- lightdm-webkit-theme-litarvan
+
+Lightdm webkit 2 looks promising
